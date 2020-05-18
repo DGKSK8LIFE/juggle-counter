96bcd4ff62c5838b607053d811d65347e1868115
Jose Santiago <gallojsantiago@gmail.com>
2020-05-18 08:01:50 -0500

    messing around with threshold

--- counter.py
+++ counter.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # Boy in sample.mp4 does 158 juggles
 
 cv2.namedWindow("preview")
-capture = cv2.VideoCapture("sample.mp4")
+capture = cv2.VideoCapture("sample_medium.mp4")
 
 circle_y = []
 
@@ -14,6 +14,9 @@ if capture.isOpened(): # try to get the first frame
    ok, frame = capture.read()
 else:
    ok = False
+
+num_frames = 0
+prev_y, prev_rad = 0, 0
 
 while True :
       ok, frame = capture.read()
@@ -24,22 +27,27 @@ while True :
       img_blur = cv2.medianBlur(frame,15)
       img_gray = cv2.cvtColor(img_blur, cv2.COLOR_RGB2GRAY)
       img_cirlces = cv2.cvtColor(img_gray,cv2.COLOR_GRAY2BGR)
-      circles =   cv2.HoughCircles(img_gray, cv2.HOUGH_GRADIENT,1,120,param1=100,param2=30,minRadius=20,maxRadius=200)
-
+      circles =   cv2.HoughCircles(img_gray, cv2.HOUGH_GRADIENT, dp=1, minDist=225, param1=100, param2=30, minRadius=20, maxRadius=150)
+      
       if circles is None:
          cv2.imshow("preview", frame)
          continue
 
-      circles = np.uint16(np.around(circles))
+      circles = np.uint64(np.around(circles))
 
       for i in circles[0,:]:
-         print(i)
+         #if(abs(int(i[2]) - prev_rad)) < 15:
+         print(abs(int(i[1]) - prev_y))
          circle_y.append(i[1])
          cv2.circle(frame,(i[0],i[1]),i[2],(0,255,0),1) # draw the outer circle
          cv2.circle(frame,(i[0],i[1]),2,(0,0,255),3) # draw the center of the circle
 
+         prev_y = i[1]
+
+
 
       cv2.imshow("preview", frame)
+      num_frames += 1
 
       if cv2.waitKey(1) & 0xFF == ord('q'):
          break
@@ -52,9 +60,11 @@ peaks, _ = find_peaks(sample) # Returns the indexes of the peaks
 
 print(peaks)
 print(len(peaks))
+print(num_frames)
+print(len(circles))
 
-# plt.plot(range(len(sample)), sample)
 plt.plot(sample)
+# plt.plot(range(num_frames), sample)
 plt.plot(peaks, sample[peaks], "x")
 plt.show()
 
